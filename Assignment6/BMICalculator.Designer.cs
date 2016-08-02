8421b062b51327dd362f49fe300e7a3f883423d9
Shivya B <sbalac17@my.centennialcollege.ca>
2016-08-02 11:48:35 -0400

    Assignment 6

---
+++ Assignment6/BMICalculator.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace Assignment6
+{
+    partial class BMICalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.ImperialRadio = new System.Windows.Forms.RadioButton();
+            this.MetricRadio = new System.Windows.Forms.RadioButton();
+            this.HeightLabel = new System.Windows.Forms.Label();
+            this.WeightLabel = new System.Windows.Forms.Label();
+            this.WeightTextBox = new System.Windows.Forms.TextBox();
+            this.HeightTextBox = new System.Windows.Forms.TextBox();
+            this.BMITextBox = new System.Windows.Forms.TextBox();
+            this.ResultsTextbox = new System.Windows.Forms.TextBox();
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Location = new System.Drawing.Point(12, 443);
+            this.CalculateButton.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(628, 55);
+            this.CalculateButton.TabIndex = 5;
+            this.CalculateButton.Text = "Calculate BMI";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.ImperialRadio, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.MetricRadio, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.HeightLabel, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.WeightLabel, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.WeightTextBox, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.HeightTextBox, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 137);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(787, 296);
+            this.tableLayoutPanel1.TabIndex = 9;
+            // 
+            // ImperialRadio
+            // 
+            this.ImperialRadio.AutoSize = true;
+            this.ImperialRadio.Location = new System.Drawing.Point(401, 7);
+            this.ImperialRadio.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.ImperialRadio.Name = "ImperialRadio";
+            this.ImperialRadio.Size = new System.Drawing.Size(128, 35);
+            this.ImperialRadio.TabIndex = 2;
+            this.ImperialRadio.Text = "Imperial";
+            this.ImperialRadio.UseVisualStyleBackColor = true;
+            // 
+            // MetricRadio
+            // 
+            this.MetricRadio.AutoSize = true;
+            this.MetricRadio.Checked = true;
+            this.MetricRadio.Location = new System.Drawing.Point(8, 7);
+            this.MetricRadio.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.MetricRadio.Name = "MetricRadio";
+            this.MetricRadio.Size = new System.Drawing.Size(106, 35);
+            this.MetricRadio.TabIndex = 1;
+            this.MetricRadio.TabStop = true;
+            this.MetricRadio.Text = "Metric";
+            this.MetricRadio.UseVisualStyleBackColor = true;
+            // 
+            // HeightLabel
+            // 
+            this.HeightLabel.AutoSize = true;
+            this.HeightLabel.Location = new System.Drawing.Point(8, 98);
+            this.HeightLabel.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.HeightLabel.Name = "HeightLabel";
+            this.HeightLabel.Size = new System.Drawing.Size(136, 31);
+            this.HeightLabel.TabIndex = 7;
+            this.HeightLabel.Text = "My Height";
+            // 
+            // WeightLabel
+            // 
+            this.WeightLabel.AutoSize = true;
+            this.WeightLabel.Location = new System.Drawing.Point(8, 196);
+            this.WeightLabel.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.WeightLabel.Name = "WeightLabel";
+            this.WeightLabel.Size = new System.Drawing.Size(141, 31);
+            this.WeightLabel.TabIndex = 7;
+            this.WeightLabel.Text = "My Weight";
+            // 
+            // WeightTextBox
+            // 
+            this.WeightTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.WeightTextBox.Location = new System.Drawing.Point(396, 199);
+            this.WeightTextBox.Name = "WeightTextBox";
+            this.WeightTextBox.Size = new System.Drawing.Size(388, 38);
+            this.WeightTextBox.TabIndex = 4;
+            this.WeightTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.NumericTextBox_KeyDown);
+            this.WeightTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.NumericTextBox_KeyPress);
+            // 
+            // HeightTextBox
+            // 
+            this.HeightTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.HeightTextBox.Location = new System.Drawing.Point(396, 101);
+            this.HeightTextBox.Name = "HeightTextBox";
+            this.HeightTextBox.Size = new System.Drawing.Size(388, 38);
+            this.HeightTextBox.TabIndex = 3;
+            this.HeightTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.NumericTextBox_KeyDown);
+            this.HeightTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.NumericTextBox_KeyPress);
+            // 
+            // BMITextBox
+            // 
+            this.BMITextBox.Enabled = false;
+            this.BMITextBox.Location = new System.Drawing.Point(12, 508);
+            this.BMITextBox.Name = "BMITextBox";
+            this.BMITextBox.Size = new System.Drawing.Size(787, 38);
+            this.BMITextBox.TabIndex = 10;
+            // 
+            // ResultsTextbox
+            // 
+            this.ResultsTextbox.Enabled = false;
+            this.ResultsTextbox.Location = new System.Drawing.Point(12, 552);
+            this.ResultsTextbox.Multiline = true;
+            this.ResultsTextbox.Name = "ResultsTextbox";
+            this.ResultsTextbox.Size = new System.Drawing.Size(787, 177);
+            this.ResultsTextbox.TabIndex = 11;
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Location = new System.Drawing.Point(651, 443);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(145, 55);
+            this.ResetButton.TabIndex = 12;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(382, 63);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "BMI Calculator";
+            // 
+            // BMICalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(811, 741);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.ResultsTextbox);
+            this.Controls.Add(this.BMITextBox);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.CalculateButton);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.MaximizeBox = false;
+            this.Name = "BMICalculator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BMI Calculator";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label HeightLabel;
+        private System.Windows.Forms.RadioButton ImperialRadio;
+        private System.Windows.Forms.RadioButton MetricRadio;
+        private System.Windows.Forms.Label WeightLabel;
+        private System.Windows.Forms.TextBox WeightTextBox;
+        private System.Windows.Forms.TextBox HeightTextBox;
+        private System.Windows.Forms.TextBox BMITextBox;
+        private System.Windows.Forms.TextBox ResultsTextbox;
+        private System.Windows.Forms.Button ResetButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Assignment6/BMICalculator.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Shivya Balachandran
+// 300 884 989
+// 08/02/16 
+
+namespace Assignment6
+{
+    /**
+  * <summary>
+  * This class contains the BMI calculator 
+  * </summary>
+  * 
+  * @class BMICalculator
+  */
+    public partial class BMICalculator : Form
+    {
+        private bool _blockKeyPress;
+
+        /**
+     * <summary>
+     * This constructor builds the BMI caclutator form 
+     * </summary>
+     * 
+     * @constructor
+     */
+        public BMICalculator()
+        {
+            InitializeComponent();
+        }
+        /**
+     * <summary>
+     * This event handler calculates the BMI 
+     * </summary>
+     * 
+     * @method CalculateButton_Click
+     * @param {object} sender
+     * @param {EventArgs} e
+     */
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            string heightString = HeightTextBox.Text;
+            double height;
+            try
+            {
+                height = Convert.ToDouble(heightString);
+            }
+            catch
+            {
+                MessageBox.Show("Invalid height");
+                return;
+            }
+            string weightString = WeightTextBox.Text;
+            double weight;
+            try
+            {
+                weight = Convert.ToDouble(weightString);
+            }
+            catch
+            {
+                MessageBox.Show("Invalid weight");
+                return;
+            }
+            double bmi;
+
+            if (ImperialRadio.Checked)
+            {
+                bmi = (weight*703) / (height*height);
+            }
+            else
+            {
+                bmi = (weight) / (height * height);
+            }
+
+            BMITextBox.Text = string.Format("Your BMI is {0:f2}.", bmi);
+        }
+
+        /**
+      * <summary>
+      * This event handler resets the form
+      * </summary>
+      * 
+      * @method ResetButton_Click
+      * @param {object} sender
+      * @param {EventArgs} e
+      */
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            BMITextBox.Text = "";
+            WeightTextBox.Text = "";
+            HeightTextBox.Text = "";
+        }
+
+        /**
+        * <summary>
+        * This event handler checks keys to see if they are allowed 
+        * </summary>
+        * 
+        * @method NumericTextBox_KeyDown
+        * @param {object} sender
+        * @param {KeyEventArgs} e
+        */
+        private void NumericTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            _blockKeyPress = false;
+
+            // block ctrl and shift
+            if (e.Control || e.Shift)
+            {
+                _blockKeyPress = true;
+                return;
+            }
+
+            // allow number keys
+            if (e.KeyCode >= Keys.D0 && e.KeyCode <= Keys.D9)
+            {
+                return;
+            }
+
+            // allow numpad keys
+            if (e.KeyCode >= Keys.NumPad0 && e.KeyCode <= Keys.NumPad9)
+            {
+                return;
+            }
+
+            // allow period
+            if (e.KeyCode == Keys.OemPeriod)
+            {
+                return;
+            }
+
+            // allow backspace and delete
+            if (e.KeyCode == Keys.Back || e.KeyCode == Keys.Delete)
+            {
+                return;
+            }
+
+            // allow left and right arrow keys
+            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.Right)
+            {
+                return;
+            }
+
+            _blockKeyPress = true;
+        }
+        /**
+     * <summary>
+     * This event blocks key presses if they were not allowed
+     * </summary>
+     * 
+     * @method NumericTextBox_KeyPress
+     * @param {object} sender
+     * @param {KeyPressEventArgs} e
+     */
+        private void NumericTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = _blockKeyPress;
+        }
+    }
+}
